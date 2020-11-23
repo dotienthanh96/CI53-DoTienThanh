a76afb41071f296c3c3e65ef6c399ec56a75a379
thanh <dotienthanh96hn@gmail.com>
2020-11-23 21:25:15 +0700

    add

---
+++ hwork/index.js
@@ -0,0 +1,15 @@
+console.log('Bài 1');
+console.log('Lập trình hướng đối tượng (OOP) là một kỹ thuật lập trình cho phép lập trình viên tạo ra các đối tượng trong code trừu tượng hóa các đối tượng.')
+console.log('4 tính chất là : ')
+console.log("Tính đóng gói (Encapsulation)")
+console.log("Tính kế thừa (Inheritance)")
+console.log("Tính đa hình (Polymorphism)")
+console.log("Tính trừu tượng (Abstraction)")
+
+console.log("Bài 2")
+
+let a = [ ] ; 
+let b = Number(prompt("Nhập Số"))
+function num() {
+    
+}
